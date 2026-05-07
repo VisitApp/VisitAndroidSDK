1d66c721834b9557f7e5fa3fd3988ceec1d5efeb
chinmay <chinmaydash11@gmail.com>
2026-05-07 14:20:32 +0530

    updated sdk version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
 
     implementation(project(":visit"))
 //     implementation(files("visit-debug.aar"))
-//    implementation("com.github.VisitApp:VisitAndroidSDK:v1.89")
+//    implementation("com.github.VisitApp:VisitAndroidSDK:v1.93")
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 
