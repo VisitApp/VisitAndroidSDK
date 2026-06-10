7f45d5453a49072a065e8f3c48656d917eeeac39
chinmay <chinmaydash11@gmail.com>
2026-06-10 17:28:58 +0530

    library version bump.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
 
 //    implementation(project(":visit"))
     // implementation(files("visit-debug.aar"))
-    implementation("com.github.VisitApp:VisitAndroidSDK:v3.1")
+    implementation("com.github.VisitApp:VisitAndroidSDK:v3.3")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
     implementation("androidx.core:core-ktx:1.17.0")
